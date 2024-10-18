a14833720ea862b79126ee6a343d517efb6a2eb8
Sebastian-G00 <sebastian.20.olave@gmail.com>
2024-10-18 13:17:56 -0500

    Add files via upload

---
+++ Proyecto_Biblioteca/Models/Autor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Biblioteca.Models
+{
+    public class Autor
+    {
+        public int AutorID { get; set; }
+
+        [Required(ErrorMessage = "El nombre del autor es obligatorio.")]
+        public string Nombre { get; set; }
+
+        public virtual ICollection<Libro> Libros { get; set; }
+    }
+}
---
+++ Proyecto_Biblioteca/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Biblioteca.Models
+{
+    public class Libro
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio.")]
+        public string Titulo { get; set; }
+
+        public int AutorID { get; set; }
+        public virtual Autor Autor { get; set; }
+    }
+}
